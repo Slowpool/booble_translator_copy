0c7882b6b88e015a0218e846f727bae2c572981c
azgel <azgel@DESKTOP-2K9D8KE>
2024-04-04 09:17:44 +0300

    idk idk

--- lib/HTTP_requester.dart
+++ lib/HTTP_requester.dart
@@ -1,24 +1,24 @@
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+
+// import '../models/language.dart';
+// import '../components/language-list-element.dart';
 
 class HTTP_requester {
 
   
 
-  // TODO implement get translation
   static String getTranslation(String text) {
     return 'translation: $text';
   }
 
+
   static getPronunciation() => Object();
 
-  // static getExamplesFor(text) => [
-  //       'first example: $text',
-  //       'sexond example: $text',
-  //       'third example: $text',
-  //       'fourth example: $text',
-  //     ];
-
-  static getExamplesFor(text) {
-    http.get();
-  }
+  static getExamplesFor(text) => [
+        'first example: $text',
+        'sexond example: $text',
+        'third example: $text',
+        'fourth example: $text',
+      ];
 }
